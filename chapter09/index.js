2d743eb33e79283ddb407ffa79c37f7b7e2a7552
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-07-16 00:44:31 +0900

    2020/07/16 #
    반응 call stack

---
+++ chapter09/index.js
@@ -0,0 +1,40 @@
+const screen = document.querySelector(".screen");
+
+let times = [];
+
+let startTime;
+let cancleTime;
+
+screen.addEventListener("click", function (e) {
+  if (screen.classList.contains("waiting")) {
+    screen.classList.remove("waiting");
+    screen.classList.add("ready");
+    screen.textContent = "초록색이 되면 클릭";
+
+    cancleTime = setTimeout(function () {
+      startTime = new Date();
+      screen.click();
+    }, Math.floor(Math.random()) * 1000 + 2000);
+  } else if (screen.classList.contains("ready")) {
+    if (!startTime) {
+      //너무 빨리 누름
+      clearTimeout(cancleTime);
+      screen.classList.remove("ready");
+      screen.classList.add("waiting");
+      screen.textContent = "너무 빨리 클릭함 다시 시도";
+    } else {
+      screen.classList.remove("ready");
+      screen.classList.add("now");
+      screen.textContent = "클릭";
+    }
+  } else if (screen.classList.contains("now")) {
+    screen.classList.remove("now");
+    screen.classList.add("waiting");
+    screen.textContent = "클릭해서 시작";
+
+    const endTime = new Date();
+    const result = endTime - startTime;
+    times.push(result);
+    console.log(result / 1000);
+  }
+});
