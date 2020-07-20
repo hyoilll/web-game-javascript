d159e4102382d26f30e2b3fc0bdad6857cece76b
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-07-21 00:19:48 +0900

    2020/07/21 #12 ...ing

--- chapter12/index.js
+++ chapter12/index.js
@@ -1,8 +1,16 @@
 const body = document.body;
+
 const rival_hero = document.querySelector(".rival-hero");
 const rival_deck = document.querySelector(".rival-deck");
+const rival_field = document.querySelector(".rival-cards");
+const rival_cost = document.querySelector(".rival-cost");
+
 const my_hero = document.querySelector(".my-hero");
 const my_deck = document.querySelector(".my-deck");
+const my_field = document.querySelector(".my-cards");
+const my_cost = document.querySelector(".my-cost");
+
+const turnBtn = document.querySelector(".turn");
 
 //deck data 저장
 let rival_deck_datas = [];
@@ -17,13 +25,46 @@ let rival_field_datas = [];
 let my_field_datas = [];
 
 //turn
+//true : my
+//false : rival
 let turn = true;
 
+//btn - click event => 턴 넘기가
+turnBtn.addEventListener("click", function (e) {
+  //턴을 넘길 때 마다 코스트를 채워줌
+  if (turn) {
+    //내 코스트 채움
+    my_cost.textContent = 10;
+  } else {
+    //상대 코스트 채움
+    rival_cost.textContent = 10;
+  }
+
+  //공수교대
+  turn = !turn;
+
+  if (turn) {
+    //내 턴이면 버튼 text가 my-turn
+    turnBtn.textContent = "MyTurn";
+  } else {
+    turnBtn.textContent = "RivalTurn";
+  }
+});
+
+//카드 - 돔 연결 해제
+function disconnetionCardToDom(card, dom) {
+  dom.querySelectorAll(".card").forEach(function (e) {
+    if (e === card) {
+      e.remove();
+    }
+  });
+}
+
 //카드 - 돔 연결
-function connectCardToDom(data, dom, IsHero) {
+function connectCardToDom(data, dom, isHero) {
   let card;
 
-  if (IsHero) {
+  if (isHero) {
     card = document.querySelector(".hero .card").cloneNode(true);
 
     const hero_name = document.createElement("div");
@@ -41,7 +82,67 @@ function connectCardToDom(data, dom, IsHero) {
     card.querySelector(".card-hp").textContent = data.hp;
   }
 
-  card.addEventListener("click", function () {});
+  card.addEventListener("click", function (e) {
+    const parentNodeName = card.parentNode.className;
+    //deck에 있는 카드만 필드에 올라갈 수 있도록해줌
+    if (parentNodeName === "my-deck" || parentNodeName === "rival-deck") {
+      let idx = 0;
+      let cost = 0;
+      if (turn) {
+        cost = Number(my_cost.textContent);
+        //내 턴인데 상대 카드를 누르면 그대로 종료
+        //현재 cost가 카드의 cost보다 적으면 소환 불가
+        if (!data.isTurn || cost < data.cost || data.field) {
+          return;
+        }
+
+        //data에 맞는 index를 찾아서 datas[]에서 삭제
+        idx = my_deck_datas.indexOf(data);
+        my_deck_datas.splice(idx, 1);
+
+        //화면에서 deck 카드 삭제
+        disconnetionCardToDom(card, my_deck);
+
+        //field에 추가
+        my_field_datas.push(card);
+        connectCardToDom(data, my_field, false);
+
+        //deck에서 감소된 만큼 다시 deck 카드 충원해줌
+        const newCard = createCard(false, true);
+        my_deck_datas.push(newCard);
+        connectCardToDom(newCard, my_deck, false);
+
+        //cost 감소
+        my_cost.textContent = String(cost - Number(data.cost));
+      } else {
+        cost = Number(rival_cost.textContent);
+        //상대 턴인데 내 카드를 누르면 그대로 종료
+        //현재 cost가 카드의 cost보다 적으면 소환 불가
+        if (data.isTurn || cost < data.cost) {
+          return;
+        }
+
+        //data에 맞는 index를 찾아서 datas[]에서 삭제
+        idx = rival_deck_datas.indexOf(data);
+        rival_deck_datas.splice(idx, 1);
+
+        //화면에서 deck 카드 삭제
+        disconnetionCardToDom(card, rival_deck);
+
+        //field에 추가
+        rival_field_datas.push(card);
+        connectCardToDom(data, rival_field, false);
+
+        //deck에서 감소된 만큼 다시 deck 카드 충원해줌
+        const newCard = createCard(false, false);
+        rival_deck_datas.push(newCard);
+        connectCardToDom(newCard, rival_deck, false);
+
+        //cost 감소
+        rival_cost.textContent = String(cost - Number(data.cost));
+      }
+    }
+  });
 
   dom.appendChild(card);
 }
@@ -49,7 +150,7 @@ function connectCardToDom(data, dom, IsHero) {
 //상대덱생성
 function createRivalDeck(cnt) {
   for (let i = 0; i < cnt; ++i) {
-    const deck = createCard();
+    const deck = createCard(false, false);
     rival_deck_datas.push(deck);
   }
 
@@ -60,7 +161,7 @@ function createRivalDeck(cnt) {
 
 //상대영웅생성
 function createRivalHero() {
-  const hero = createCard(true);
+  const hero = createCard(true, false);
   rival_hero_data = hero;
 
   connectCardToDom(rival_hero_data, rival_hero, true);
@@ -69,7 +170,7 @@ function createRivalHero() {
 //내덱생성
 function createMyDeck(cnt) {
   for (let i = 0; i < cnt; ++i) {
-    const deck = createCard();
+    const deck = createCard(false, true);
     my_deck_datas.push(deck);
   }
 
@@ -80,15 +181,15 @@ function createMyDeck(cnt) {
 
 //내영웅 생성
 function createMyHero() {
-  const hero = createCard(true);
+  const hero = createCard(true, true);
   my_hero_data = hero;
 
   connectCardToDom(my_hero_data, my_hero, true);
 }
 
 //생성자 패턴
-function Card(hero) {
-  if (hero) {
+function Card(isHero, isTurn) {
+  if (isHero) {
     this.att = Math.ceil(Math.random() * 2);
     this.hp = Math.ceil(Math.random() * 5) + 25;
     this.hero = true;
@@ -97,13 +198,19 @@ function Card(hero) {
     this.hp = Math.ceil(Math.random() * 5);
     this.cost = Math.floor((this.att + this.hp) / 2);
   }
+
+  if (isTurn === true) {
+    this.isTurn = true;
+  } else {
+    this.isTurn = false;
+  }
 }
 
 //팩토리 패턴
 //카드공장
 //호출하는 곳에서 매개변수를 넘기지 않으면 undefined가 넘어오므로 자동으로 false
-function createCard(hero) {
-  return new Card(hero);
+function createCard(isHero, isTurn) {
+  return new Card(isHero, isTurn);
 }
 
 //초기세팅
